81b21fe9c2be86400abf5e728d7ae65ac42879c1
zlchkust <862928150@qq.com>
2018-01-21 10:28:18 +0800

    add

--- 20180120_Licheng_Module1.py
+++ 20180120_Licheng_Module1.py
@@ -5,7 +5,10 @@ import pandas as pd
 import numpy as np
 from sklearn.neural_network import MLPRegressor
 import xgboost as xgb
-import lightgbm as lgb
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.metrics import make_scorer
+from sklearn.model_selection import cross_val_score
+from sklearn.grid_search import GridSearchCV
 
 trainPredFile = "C:\Users\lzhaoai\Desktop\predict_weather\ForecastDataforTraining_201712.csv"
 trainTrueFile = "C:\Users\lzhaoai\Desktop\predict_weather\In_situMeasurementforTraining_201712.csv"
@@ -17,47 +20,61 @@ testPredFile = "C:\Users\lzhaoai\Desktop\predict_weather\ForecastDataforTesting_
 
 #X_train,y_train = model4(trainPredFile, trainTrueFile, testPredFile)
 
-def my_custom_loss_func(ground_truth, predictions):
+def my_custom_loss_func1(ground_truth, predictions):
     ground_truth = map(lambda x: 1 if x>=15 else 0, ground_truth)
     predictions = map(lambda x: 1 if x>=15 else 0, predictions)
     diff = [ground_truth[i] - predictions[i] for i in range(len(ground_truth))]
-    return reduce(lambda x,y: x+y, map(lambda x: 1 if x!=0 else 0, diff))*1.0/len(ground_truth)
-ground_truth = [2,3,44,6]
-predictions = [5,23,5,7]
-print my_custom_loss_func(ground_truth, predictions)    
-#    diff = np.abs(ground_truth - predictions).max()
-#    return np.log(1 + diff)
+    return reduce(lambda x,y: x+y, map(lambda x: 1 if x==0 else 0, diff))*1.0/len(ground_truth)
+def my_custom_loss_func2(ground_truth, predictions):
+    ground_truth = map(lambda x: 1 if x>=0.5 else 0, ground_truth)
+    predictions = map(lambda x: 1 if x>=0.5 else 0, predictions)
+    diff = [ground_truth[i] - predictions[i] for i in range(len(ground_truth))]
+    return reduce(lambda x,y: x+y, map(lambda x: 1 if x==0 else 0, diff))*1.0/len(ground_truth)
+
+score1 = make_scorer(my_custom_loss_func1, greater_is_better=True) 
+score2 = make_scorer(my_custom_loss_func2, greater_is_better=True) 
+
+X_train_copy = X_train.copy()
+y_train_copy = y_train.copy()
+X_train_copy[:,4:] = 1/(1+np.exp(-(X_train_copy[:,4:]-15)))
+y_train_copy = 1/(1+np.exp(-(y_train_copy-15)))
 
 #xgbr = xgb.XGBRegressor()
-#print 'Score for XGBoosting :',cross_val_score(xgbr,X_train,y_train,cv=10,scoring='accuracy').mean()
+#param_grid = {'max_depth':[3,4,5,6],
+#              'learning_rate': [0.003,0.03,0.3], 
+#              'reg_alpha': [ 0.001, 0.01, 0.1, 1],
+#              'reg_lambda': [ 0.01, 0.1, 1, 5]}
+#grid_search_xgbr = GridSearchCV(xgbr, param_grid=param_grid, scoring=score,cv=10,verbose = 2).fit(X_train,y_train)
+#xgbr_best = grid_search_xgbr.best_estimator_
+#print 'Score for XGBoosting :',cross_val_score(xgbr_best,X_train,y_train,cv=10,scoring=score).mean()
+##print 'Score for XGBoosting :',cross_val_score(xgbr,X_train[:,range(2)+range(3,14)],y_train,cv=10,scoring=score).mean()
+
+#adb = AdaBoostRegressor()
+##param_grid = {'learning_rate': [0.3,1,3,10], 
+##              'n_estimators': [50,100,200],
+##              'loss': ['linear', 'square', 'exponential']}
+#param_grid = {'learning_rate': list(np.linspace(0.5,3,6)), 
+#              'n_estimators': [50,100,200],
+#              'loss': ['linear', 'square']}
+#grid_search_adb = GridSearchCV(adb, param_grid=param_grid, scoring=score,cv=10,verbose = 2).fit(X_train,y_train)
+#adb_best = grid_search_adb.best_estimator_
+#print 'Score for AdaBoosting :',cross_val_score(adb_best,X_train,y_train,cv=10,scoring=score).mean()
 
 
-
-#clf_p = MLPRegressor(random_state = 0)
-#predict = pd.DataFrame(columns = ["xid","yid","date_id","hour","wind"])
-#for i in range(90): 
-#    X_train = df_train.get_chunk(chunksize).drop(["date_id","hour"], axis = 1)
-#    X_train = pd.get_dummies(X_train, columns = ["model"]).values
-#    X_test = df_test.get_chunk(chunksize).drop(["date_id","hour"], axis = 1)
-#    X_test = pd.get_dummies(X_test, columns = ["model"]).values
-#    Data= df_train_true.get_chunk(chunksize / 10).reset_index(drop=True)
-#    y_train = Data["wind"].values.reshape(-1,1)
-#    Data.drop(["wind"],axis = 1,inplace = True)
-#    y_train_org = y_train.copy().ravel()
-#    y_train = np.kron(y_train,np.ones(10).reshape(-1,1)).ravel()
-#    rs = ShuffleSplit(n_splits=1, test_size=.25, random_state=0)
-#    for train_index, val_index in rs.split(X_train):
-#        X_train_train,y_train_train = X_train[train_index],y_train[train_index]
-#        X_train_val,y_train_val = X_train[val_index],y_train[val_index]
-#        clf_p.partial_fit(X_train_train,y_train_train)
-#        insamplemse = ((clf_p.predict(X_train_train) - y_train_train)**2).mean()
-#        outsamplemse = ((clf_p.predict(X_train_val) - y_train_val)**2).mean()
-#        y_pred = clf_p.predict(X_train)
-#        y_pred = np.mean(y_pred.reshape(-1,10),axis = 1)
-#        allsamplemse = ((y_pred - y_train_org)**2).mean()
-#        print "day "+str(i/18)+", hour "+str(3+i%18)+", total MSE: "+str("{:.4f}".format(insamplemse))\
-#        +" "+str("{:.4f}".format(outsamplemse))+" "+str("{:.4f}".format(allsamplemse))
-#    y_test = clf_p.predict(X_test)
-#    y_test = np.mean(y_test.reshape(-1,10),axis = 1)
-#    wind = pd.DataFrame(y_test,columns = ["wind"])
-#    predict = predict.append(pd.concat((Data,wind),axis = 1),ignore_index = True)    
+xgbr = xgb.XGBRegressor()
+print 'Score for XGBoosting :',cross_val_score(xgbr,X_train,y_train,cv=3,scoring=score1).mean()
+xgbr = xgb.XGBRegressor()
+#param_grid = {'max_depth':[3,4,5,6],
+#              'learning_rate': [0.003,0.03,0.3], 
+#              'reg_alpha': [ 0.001, 0.01, 0.1, 1],
+#              'reg_lambda': [ 0.01, 0.1, 1, 5]}
+param_grid = {'max_depth':[3,4,5,6,7],
+              'learning_rate': [0.0003,0.003,0.03], 
+              'reg_alpha': [0.1,1,5,10],
+              'reg_lambda': range(1,10),
+              'objective': ['reg:logistic','reg:linear']}
+grid_search_xgbr = GridSearchCV(xgbr, param_grid=param_grid, scoring=score2,cv=3,verbose = 2).fit(X_train_copy,y_train_copy)
+xgbr_best = grid_search_xgbr.best_estimator_
+print 'Score for XGBoosting :',cross_val_score(xgbr_best,X_train_copy,y_train_copy,cv=3,scoring=score2).mean()
+#adb = AdaBoostRegressor()
+#print 'Score for AdaBoosting :',cross_val_score(adb,X_train_copy,y_train_copy,cv=3,scoring=score2).mean()
