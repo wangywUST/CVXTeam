f4b010bf7838b922862d93839a9b1e180cabb461
zlchkust <862928150@qq.com>
2018-02-04 16:59:12 +0800

    add

--- Functions/Licheng/Module3_func.py
+++ Functions/Licheng/Module3_func.py
@@ -70,49 +70,43 @@ def obtainScore(submitfile, weatherfile,cityLocFile,xsize = 548,ysize = 421,maxD
                 Score += [score]
     return Score  
 
-def plotweather(submitfile, weatherfile,cityLocFile,xsize = 548,ysize = 421,maxDay = 5,maxCity = 10,threshold = 15):
+def plot_func(dayNum,city,pathpiece,windGraph,xCity,yCity,hourNum,seg,X,Y):
+    for j in range(min(len(seg),hourNum)):  
+        print "Day: "+str(dayNum+5) + ", Hour: "+ str(j + 3) + ", City: " + str(city)
+        windGraph_j = windGraph[j, :, :].copy()
+        windGraph_j[windGraph_j < 15] = 0
+        windGraph_j[windGraph_j >= 15] = 1
+        plt.scatter(yCity[1:11], xCity[1:11], marker='x', s=50, c = 'gold', zorder=10)
+        plt.scatter(yCity[0], xCity[0], marker='*', s=50, c = 'gold', zorder=10)
+        plt.scatter(pathpiece["y"], pathpiece["x"], marker='x', s=1, c = 'gold', zorder=10)
+        plt.scatter(pathpiece["y"][sum(seg[:j]):sum(seg[:j+1])], pathpiece["x"][sum(seg[:j]):sum(seg[:j+1])], \
+            marker='x', s=1, c = 'g', zorder=10)
+        CSF = plt.contourf(X, Y, windGraph_j, 8, alpha=.95, cmap=plt.cm.Greys)
+        plt.colorbar(CSF, shrink=0.8, extend='both')
+        plt.title("Day: "+str(dayNum+5) + ", Hour: "+ str(j + 3) + ", City: " + str(city))
+        plt.savefig('Figure/Licheng/' + str(dayNum+5) + ' Day ' + str(city) + ' City ' + str(j + 3) + ' Hour.pdf')
+        plt.clf()
+        print "Done!"
+
+def plotweather(submitfile, weatherfile,cityLocFile,hourNum = 18,xsize = 548,ysize = 421,maxDay = 5,maxCity = 10,threshold = 15):
     cityloc = pd.read_csv(cityLocFile)
     xCity = cityloc['xid']
     yCity = cityloc['yid']
-    chunksize = xsize * ysize
-    pathfile = pd.read_csv(submitfile,header = None, names = ["city","Day","Time","x","y"])
-    windGraph = np.zeros((18,xsize,ysize))
     x = np.linspace(1, xsize, xsize)
     y = np.linspace(1, ysize, ysize)
     X,Y = np.meshgrid(y, x)
+    pathfile = pd.read_csv(submitfile,header = None, names = ["city","Day","Time","x","y"])        
     for dayNum in range(1,maxDay+1):
-        df = pd.read_csv(weatherfile, chunksize = chunksize)
-        df = jumpDays(df, dayNum-1, chunksize)
-        for _ in range(18):
-            windGra = df.get_chunk(chunksize)["wind"]
-            windGraph[_,:,:] = windGra.values.reshape(xsize,ysize).copy()
+        windGraph = getWindGraph(weatherfile,dayNum)
         for city in range(1,maxCity+1):
-            pathpiece = pathfile.loc[(pathfile["city"] == city) & (pathfile["Day"] == dayNum + 5)][["x","y"]].reset_index(drop = True)
+            pathpiece = getpathpiece(pathfile,city,dayNum)
             Len = pathpiece.shape[0]
-            if(Len == 0):  continue
-            seg = []
-            while(Len>=30):
-                seg += [30]
-                Len -= 30
-            if(Len > 0):
-                seg += [Len]
-            for j in range(min(len(seg),18)):  
-                windGraph1 = windGraph[j, :, :].copy()
-                windGraph1[windGraph[j, :, :] >= 15] = 10
-                windGraph1[windGraph[j, :, :] < 15] = 0
-#                CS = plt.contour(X, Y, windGraph1, levels = [10],colors=('k',),linestyles=('-',),linewidths=(1,))
-
-                plt.scatter(yCity[1:11], xCity[1:11], marker='x', s=50, c = 'gold', zorder=10)
-                plt.scatter(yCity[0], xCity[0], marker='*', s=50, c = 'gold', zorder=10)
-                plt.scatter(pathpiece["y"], pathpiece["x"], marker='x', s=1, c = 'gold', zorder=10)
-                plt.scatter(pathpiece["y"][sum(seg[:j]):sum(seg[:j+1])], pathpiece["x"][sum(seg[:j]):sum(seg[:j+1])], marker='x', s=1, c = 'g', zorder=10)
-                
-                CSF = plt.contourf(X, Y, windGraph1, 8, alpha=.95, cmap=plt.cm.Greys)
-#                plt.clabel(CS, fmt = '%2.1d', colors = 'k', fontsize=14) #contour line labels
-                plt.colorbar(CSF, shrink=0.8, extend='both')
-                
-                plt.title(str(dayNum+5) + ' Day ' + str(city) + ' City ' + str(j + 3) + ' Hour ')
-                plt.savefig('Figure/Licheng/' + str(dayNum+5) + ' Day ' + str(city) + ' City ' + str(j + 3) + ' Hour.png')
-                plt.clf()
+            if Len > 0: 
+                plot_func(dayNum,city,pathpiece,windGraph,xCity,yCity,hourNum,partition(Len),X,Y)
+                    
+                    
+                    
+                    
+                    
     
 
