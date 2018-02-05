9c61cc2305c99c4a01f58aa433de67ad7f76774e
zlchkust <862928150@qq.com>
2018-02-05 21:42:27 +0800

    add

--- 20180129_Licheng_Module3.py
+++ 20180129_Licheng_Module3.py
@@ -12,7 +12,7 @@ from Module3_func import *
 
 #submitfile = "C:\Users\lzhaoai\Dropbox\With Licheng and Linlong\contest\submit20180116\submitResult_20180119_2.csv"
 #submitfile = "C:\Users\lzhaoai\Desktop\Tianchi\CVXTeam\Data\submitResult_Licheng_20180204_2.csv"
-submitfile = "C:\Users\lzhaoai\Desktop\Tianchi\CVXTeam\Data\submitResult_Yiwei_20180204_1.csv"
+submitfile = "C:\Users\lzhaoai\Desktop\Tianchi\CVXTeam\Data\submitResult_Linlong_20180205.csv"
 weatherfile = "C:\Users\lzhaoai\Desktop\predict_weather\predict_model_2.csv"
 cityLocFile = "C:\Users\lzhaoai\Desktop\predict_weather\CityData.csv"
 #submitfile = None
